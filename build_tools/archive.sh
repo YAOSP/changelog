35665f9df5c636f8aefc6a35f644fe506ade6012
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-05-04 16:24:07 +0200

    archive: add to repo
    as a backup, this script goes above the source root folder and extracts all non AOSP repos as tarballs
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

---
+++ build_tools/archive.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+start_time=`date +'%d/%m/%y %H:%M:%S'`
+
+src_pwd="`pwd`/$1"
+dst_pwd="`pwd`/$2"
+
+echo "----------------------------------------------"
+echo "Archiving non-AOSP repos"
+echo "----------------------------------------------"
+
+if [ "$1" != "" ]; then
+  if [ "$2" != "" ]; then
+    cd $src_pwd
+    tar cvf external_busybox.tar external/busybox
+    tar cvf external_exfat.tar external/exfat
+    tar cvf external_e2fsprogs.tar external/e2fsprogs
+    tar cvf external_fuse.tar external/fuse
+    tar cvf external_ntfs-3g.tar external/ntfs-3g
+    tar cvf kernel.tar kernel
+    tar cvf manifest.tar manifest
+    tar cvf vendor.tar vendor
+    tar cvf yaosp.tar yaosp
+    mv $src_pwd/*.tar $dst_pwd
+    echo " "
+    echo "Started  : $start_time"
+    echo "Finished : `date +'%d/%m/%y %H:%M:%S'`"
+    echo " "
+    echo "Source      : $src_pwd"
+    echo "Destination : $dst_pwd"
+    exit
+  fi
+fi
+echo "Usage: archive [source] [destination]"
